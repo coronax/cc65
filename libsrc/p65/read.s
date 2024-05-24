c2a027ebb9104795223a3fa1a2d4a0c3150ed44f
coronax <coronax@gmail.com>
2024-05-24 00:03:04 -0500

    Better counting in _read, better checking of file status, and much cleanup.

--- libsrc/p65/read.s
+++ libsrc/p65/read.s
@@ -1,4 +1,5 @@
-; Project:65 C Library
+; Project:65 library -- Christopher Just
+;
 ; int __fastcall__ read (int fd, void* buffer, int count)
 ;
 
@@ -6,6 +7,7 @@
         .importzp sp, ptr1, tmp1, tmp2, tmp3
         .import popax
  	    .include "p65.inc"
+        .include "fcntl.inc"
 
 ; It looks like this read() is closely modeled on the Unix version. So
 ; that means it will read up to count characters into buffer, and return
@@ -20,92 +22,57 @@
 ; We're going to use cc65's ptr1 for the buffer address
 ; We'll use tmp1/2 to store count, Y and tmp3 to store result
 .proc _read
-        ;pha
-        ;phx             ; Save count to the stack for now
         cmp #0
         bne nonzero
         cpx #0
         bne nonzero
-        rts             ; count is 0, just return?
+        rts             ; count is 0, so we can just return 0.
 nonzero:
         sta tmp1        ; low byte of count
         stx tmp2        ; high byte of count
-        ldy #0
-        lda (sp),y
-        sta ptr1
-        iny
-        lda (sp),y
-        sta ptr1+1      ; Save buffer ptr to ptr1
-        iny
-        lda (sp),y
-        jsr P65_SETDEVICE   ; set active device to fd
 
-        jsr popax ; fix up stack
         jsr popax
+        sta ptr1
+        stx ptr1+1      ; ptr1 points to buffer
+
+        jsr popax
+        jsr P65_SETDEVICE   ; set active device
 
         ; We should probably check if the file is actually open.
-        ldx		P65_DEVICE_OFFSET
-		lda		DEVTAB + DEVENTRY::FILEMODE,X
-        bne     start_read
+        ldx	P65_DEVICE_OFFSET
+		lda	DEVTAB + DEVENTRY::FILEMODE,X
+        and #O_RDONLY
+        bne start_read
         lda #$FF
         ldx #$FF
-        rts             ; return fail
+        rts             ; return -1 for failure
 start_read:
-        stz tmp3
+        stz tmp3        ; initialize read count
         ldy #0
-        ;stz tmp4
-
-        ; we need to subtract one from count, so that we terminate 
-        ; when count flips to $FFFF
-;        lda tmp1
-;        sec
-;        sbc #1
-;        sta tmp1
-;        lda tmp2
-;        sbc #0 ; just subtract the carry
-;        sta tmp2
-
+ 
 loop:
         ;lda #'+'
         ;jsr P65_PUT_CHAR
         jsr P65_DEV_GETC
         cpx #$FF
-        beq done        ; EOF
+        beq done        ; apparently we received EOF
         sta (ptr1),y    ; save character to buffer
         iny
-        bne decrement_count
-        ; y rolled over, so we need to increment ptr1h and tmp3
-        inc tmp3
-        inc ptr1+1
-decrement_count:
+        bne test_count
+        inc tmp3        ; if Y rolled over, we need to increment
+        inc ptr1+1      ; tmp3 and ptr1+1
 
-        ; subtract one from count (in tmp1/2). If nonzero, loop
-        lda tmp1
-        sec
-        sbc #1
-        sta tmp1
-        lda tmp2
-        sbc #0 ; just subtract the borrow
-        sta tmp2
+test_count:
+        ; if y/tmp3 == tmp1/tmp2, we are ready to exit.
+        cpy tmp1
         bne loop
-        lda tmp1
+        lda tmp3
+        cmp tmp2
         bne loop
 
-
-;        ldx tmp1        ; CJ BUG indexing is messed up, we're looping once too many. 
-;        dex
-;        stx tmp1
-;        cpx #$ff        ; check if tmp1 rolled under
-;        bne loop
-;        ldx tmp2
-;        dex
-;        stx tmp2
-;        cpx #$ff        ; check if tmp2 rolled under
-;        bne loop
 done:
-        tya             ; load result into AX
+        tya             ; load read count into AX
         ldx tmp3
         rts
-
 
 .endproc
